3e8e037ccd55081f8dba4d706c65e5371d70982a
Marvin Brouwer <marvin-brouwer@live.nl>
2025-01-24 18:16:50 +0100

    feat: added language routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,16 @@ import './app.css'
 import { DependencyGate } from './dependency-gate'
 import { CameraContext } from './context/camera-context'
 import { SpotifyContext } from './context/spotify-context'
+import { Component } from 'solid-js'
 
-function App() {
+export const MainApp: Component = () => <>
+	<SpotifyContext />
+	<CameraContext />
+	<DependencyGate>
+		<p>TODO</p>
+	</DependencyGate>
+</>
 
-	console.log(import.meta.env)
-	return (
-		<>
-			<SpotifyContext />
-			<CameraContext />
-			<DependencyGate>
-				<p>TODO</p>
-			</DependencyGate>
-		</>
-	)
-}
-
-export default App
+export const LandingPage: Component = () => <>
+	<p>TODO, language selector <a href="/guessify/en">English</a> / <a href="/guessify/nl">Nederlands</a></p>
+</>
--- src/i18n/dictionary.ts
+++ src/i18n/dictionary.ts
@@ -2,6 +2,7 @@ import * as i18n from "@solid-primitives/i18n"
 import { Accessor, createMemo, createSignal, JSX } from 'solid-js'
 import { default_dictionary } from './dictionaries/_default'
 import { nl_dictionary } from './dictionaries/nl'
+import { useParams } from '@solidjs/router'
 
 export type Dictionary = typeof default_dictionary
 
@@ -11,14 +12,20 @@ const dictionaries = {
 }
 export type Locale = keyof typeof dictionaries
 
-const [locale] = createSignal<Locale>("nl")
-
 export type Dictionaries = {
 	locale: Accessor<Locale>
 	dictionary: Dictionary
 	template: i18n.TemplateResolver<string | NonNullable<JSX.Element>>
 }
 export const useDictionaries: Accessor<Dictionaries> = () => {
+
+	const locale = createMemo(() => {
+		const routeParams = useParams();
+
+		return (routeParams.locale && Object.keys(dictionaries).includes(routeParams.locale))
+		? routeParams.locale as Locale
+		: 'en'
+	},useParams)
 
 	const dict = createMemo<Dictionary>(() => dictionaries[locale()])
 
