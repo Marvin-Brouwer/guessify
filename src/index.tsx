7542fa92d459be1589fb6e797a5ca1c6a238e8fd
Marvin Brouwer <marvin-brouwer@live.nl>
2025-01-28 20:02:58 +0100

    fix: hopefully fixed gh pages routing issues

--- src/index.tsx
+++ src/index.tsx
@@ -9,12 +9,15 @@ import { NetworkStatusContext } from './context/network-context'
 
 const root = document.getElementById('root')
 
+const filterGhPagesUrl = (url: string) => url
+	.replace(import.meta.env.BASE_URL + '/?', import.meta.env.BASE_URL);
+
 render(() => <>
 	<NetworkStatusContext>
 		<SpotifyApiContext>
 			{/* For some reason the solid navigation fails on the homepage */}
 			{/* So we turn on explicitLinks ans use normal anchors */}
-			<Router base={import.meta.env.BASE_URL} explicitLinks={true}>
+			<Router base={import.meta.env.BASE_URL} explicitLinks={true} transformUrl={filterGhPagesUrl}>
 				<Route path="/spotify-auth" component={SpotifyLoginRedirect} />
 				<Route path="/:locale/" component={MainApp} />
 				<Route path="/" component={LandingPage} />
