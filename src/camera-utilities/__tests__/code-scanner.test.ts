fc702204dc25a68ff1ccacecf48eecc3855de403
Marvin Brouwer <marvin-brouwer@live.nl>
2025-02-22 18:45:28 +0100

    feat: Moved code parsing to it's own file

--- src/camera-utilities/__tests__/code-scanner.test.ts
+++ src/camera-utilities/__tests__/code-scanner.test.ts
@@ -16,6 +16,7 @@ import { drawAngleDetail } from '../angle-scan.debug'
 import { redrawCode } from '../code-redraw'
 import { findBoundary } from '../boundary-scan'
 import { drawBoundaryDetail } from '../boundary-scan.debug'
+import { parseCode } from '../code-parser'
 
 fixTestEnvironment()
 
@@ -97,50 +98,3 @@ test.concurrent.for(timestamps)('scan-steps [%s]', async ([_, timestamp, expecte
 	// Assert
 	expect(code).toBe(expectedResult)
 })
-
-
-const barThreshold = 130
-
-function countColumnHeight(image: globalThis.ImageData, x: number) {
-
-	let whitePixelCount = 0
-	for (let y = 0; y < image.height; y++) {
-		const pixel = pixelDataFromOffset(image, x, y)
-		if (pixel.r >= barThreshold) {
-			whitePixelCount++
-		}
-	}
-
-	return whitePixelCount
-}
-
-function countColumn(image: globalThis.ImageData, x: number, midHeight: number) {
-
-
-	const barHeight = countColumnHeight(image, x)
-
-	const roundResult = Math.round((barHeight / midHeight) * 8) - 1
-	// Make sure negatives never happen
-	return roundResult > 0 ? roundResult : 0
-}
-
-function parseCode(codeCanvas: OffscreenCanvas | undefined) {
-	if (codeCanvas === undefined) return undefined
-
-	const codeImage = canvasConfiguration
-		.getCanvasContext(codeCanvas!)
-		.getImageData(0, 0, codeCanvas!.width, codeCanvas!.height)
-
-	const midHeight = countColumnHeight(codeImage, 22)
-
-	let code = ''
-	for (let x = 0; x <= 44; x += 2) {
-		const columnHeight = countColumn(codeImage, x, midHeight)
-		if (x === 0 && columnHeight != 0) return undefined;
-		if (x === 22 && columnHeight != 7) return undefined;
-		if (x === 44 && columnHeight != 0) return undefined;
-		code += columnHeight.toString()
-	}
-
-	return code;
-}
---
+++ src/camera-utilities/code-parser.ts
@@ -0,0 +1,49 @@
+import { canvasConfiguration } from './canvas'
+import { pixelDataFromOffset } from './pixel-grid'
+
+const barThreshold = 130
+
+function countColumnHeight(image: globalThis.ImageData, x: number) {
+
+	let whitePixelCount = 0
+	for (let y = 0; y < image.height; y++) {
+		const pixel = pixelDataFromOffset(image, x, y)
+		if (pixel.r >= barThreshold) {
+			whitePixelCount++
+		}
+	}
+
+	return whitePixelCount
+}
+
+function countColumn(image: globalThis.ImageData, x: number, midHeight: number) {
+
+
+	const barHeight = countColumnHeight(image, x)
+
+	const roundResult = Math.round((barHeight / midHeight) * 8) - 1
+	// Make sure negatives never happen
+	return roundResult > 0 ? roundResult : 0
+}
+
+export function parseCode(codeCanvas: OffscreenCanvas | undefined) {
+	if (codeCanvas === undefined) return undefined
+
+	const codeImage = canvasConfiguration
+		.getCanvasContext(codeCanvas!)
+		.getImageData(0, 0, codeCanvas!.width, codeCanvas!.height)
+
+	const midHeight = countColumnHeight(codeImage, 22)
+
+	let code = ''
+	for (let x = 0; x <= 44; x += 2) {
+		const columnHeight = countColumn(codeImage, x, midHeight)
+		// Some validation logic to make sure it's actually a spotify code
+		if (x === 0 && columnHeight != 0) return undefined;
+		if (x === 22 && columnHeight != 7) return undefined;
+		if (x === 44 && columnHeight != 0) return undefined;
+		code += columnHeight.toString()
+	}
+
+	return code;
+}
